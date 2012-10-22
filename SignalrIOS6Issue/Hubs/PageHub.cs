0a1fa13d7f5c1f6ed47d3033359f58913334f6f4
Jason More <Jason.More@gmail.com>
2012-10-22 11:01:45 -0500

    added animation on signalr load

---
+++ SignalrIOS6Issue/Hubs/PageHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SignalR.Hubs;
+
+namespace SignalrIOS6Issue.Hubs
+{
+    public class PageHub : Hub
+    {
+        public void clientInit()
+        {
+            Caller.ClientId = Caller.ClientId ?? Guid.NewGuid();
+
+            var state = new { };
+            Caller.gotClientState(state);
+
+            //Clients[_DISPLAY].gotClientConnect(state);
+        }
+    }
+}
